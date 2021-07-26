be390b9e1ae792c4d16e381eeb50ae9a61e597cd
oguzkose <oguzkose2019@outlook.com>
2021-07-26 15:37:51 +0300

    local storage added

--- counter.js
+++ counter.js
@@ -1,13 +1,18 @@
-let counter = 0;
+let counter = localStorage.getItem("counter")
+  ? Number(localStorage.getItem("counter"))
+  : 0;
+
 let counterBox = document.querySelector("#counter");
 let increase = document.querySelector("#increase");
 let decrease = document.querySelector("#decrease");
+
+counterBox.innerHTML = counter;
 
 increase.addEventListener("click", clickEvent);
 decrease.addEventListener("click", clickEvent);
 
 function clickEvent() {
-
   this.id == "increase" ? (counter += 1) : (counter -= 1);
+  localStorage.setItem("counter", counter);
   counterBox.innerHTML = counter;
 }
